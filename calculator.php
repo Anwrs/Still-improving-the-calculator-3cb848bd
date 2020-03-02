ac66ea52c2ab70e9a36d1b6b92028b9075c604d9
Anouar ElGhazaoui <2080049@talnet.nl>
2020-03-02 10:18:05 +0100

    done

--- calculator.php
+++ calculator.php
@@ -8,34 +8,34 @@
 <body>
     <h1>Calculator</h1>
     <?php 
-if (isset($_GET['submit'])) {
-    $num1 = (int) $_GET['num1'];
-    $num2 = (int) $_GET['num2'];
-    $submit = $_GET['submit'];
-    $ans = "";
+    if (isset($_GET['submit'])) {
+        $num1 = (int) $_GET['num1'];
+        $num2 = (int) $_GET['num2'];
+        $submit = $_GET['submit'];
+        $ans = "";
     
-    switch ($submit) {
-        case 'Add': 
-            $ans = $num1 + $num2;
-            break;
-        case 'Substract':
-            $ans = $num1 - $num2;
-            break;
-        case 'Multiply':
-            $ans = $num1 * $num2;
-            break;
-        case 'Divide':
-            $ans = $num1 / $num2;
-            break;
-        case 'Modulo':
-            $ans = $num1 % $num2;
-            break;
-        default:
-            $ans = "Error";
-            break;
+        switch ($submit) {
+            case 'Add': 
+                $ans = $num1 + $num2;
+                break;
+            case 'Substract':
+                $ans = $num1 - $num2;
+                break;
+            case 'Multiply':
+                $ans = $num1 * $num2;
+                break;
+            case 'Divide':
+                $ans = $num1 / $num2;
+                break;
+            case 'Modulo':
+                $ans = $num1 % $num2;
+                break;
+            default:
+                $ans = "Error";
+                break;
+        }
     }
-}
-?>
+    ?>
 
     <form action="calculator.php" method="get">
         <div style="display:flex; height:50px">
